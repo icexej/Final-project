aa8cd6292175bdb5f8b9200e6c602dd5eed05751
User <alymbekovaaisulu6a@gmail.com>
2026-04-13 10:57:10 +0600

    .

--- src/Test.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Test {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Если ты видишь это окно, значит всё работает!");
-    }
-}
